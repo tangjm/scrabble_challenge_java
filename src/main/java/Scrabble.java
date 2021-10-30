53c37668013a59c785405af7fc5dc71b700ef736
tangjm <jaredtang@hotmail.co.uk>
2021-10-30 13:05:47 +0100

    Major refactoring to ensure all methods have <= 5 lines.

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.Locale;
 
 public class Scrabble {
-    /*int[] index = {0, 1, 2, 3, 4};
-    int[] values = {1, 2, 3, 4, 5};*/
     final private static char[][] letterValues = {
             {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'},
             {'D', 'G'},
@@ -13,8 +11,7 @@ public class Scrabble {
             {'F', 'H', 'V', 'W', 'Y'},
             {'K'},
     };
-    /*int[] index2 = {0, 1};
-    int[] values2 = {8, 10};*/
+
     final private static char[][] letterValues2 = {
             {'J', 'X'},
             {'Q', 'Z'}
@@ -26,7 +23,10 @@ public class Scrabble {
     private boolean doubleWord;
     private boolean tripleWord;
 
-    // constructors
+    private byte score = 0;
+    private byte tempLetterScore = 0;
+    private char[] charArr;
+
     public Scrabble(String word) {
         this.word = word;
     }
@@ -40,69 +40,76 @@ public class Scrabble {
         this.tripleWord = tripleWord;
     }
 
-    // methods
     public byte score() {
-        byte score = 0;
+        this.checkIfNotNull();
+        return this.score;
+    }
+
+    public void checkIfNotNull() {
         if (this.word != null) {
-            char[] charArr = this.word.toUpperCase().toCharArray();
-            for (char letter : charArr) {
-                byte letterScore = letterScore2(letter);
-                score += letterScore;
-            }
-            score = applyWordBonus(score);
+            this.setCharArr();
+            this.calculateScore();
+            this.applyWordBonus();
         }
-        return score;
     }
 
-    public byte letterScore2(char letter) {
-        byte letterScore = letterScore(letter);
-        if (applyLetterBonus(letter)) {
-            if (applyCorrectLetterBonus(letter, this.doubleLetters)) {
-                letterScore *= 2;
-            } else if (applyCorrectLetterBonus(letter, this.tripleLetters)) {
-                letterScore *= 3;
-            }
-        }
-        return letterScore;
+    public void setCharArr() {
+        this.charArr = this.word.toUpperCase().toCharArray();
     }
 
-    public byte letterScore(char letter) {
-        byte letterScore;
+    public void calculateScore() {
+        for (char letter : this.charArr) {
+            letterScore(letter);
+            this.score += this.tempLetterScore;
+        }
+    }
+
+    public void letterScore(char letter) {
+        computeLetterScore(letter);
+        if (this.applyLetterBonus()) {
+           computeLetterBonus(letter);
+        }
+    }
+
+    public void computeLetterScore(char letter) {
         if (letter == 'J' || letter == 'X' || letter == 'Q' || letter == 'Z') {
-            letterScore = calculateLetterValue(letter, letterValues2);
+            computeLetterValue(letter, letterValues2);
         } else {
-            letterScore = calculateLetterValue(letter, letterValues);
+            computeLetterValue(letter, letterValues);
         }
-        return letterScore;
     }
 
-    public byte calculateLetterValue(char letter, char[][] conversionTable) {
-        byte score = 0;
+    public void computeLetterValue(char letter, char[][] conversionTable) {
         for (byte i = 0; i < conversionTable.length; i++) {
             for (byte j = 0; j < conversionTable[i].length; j++) {
-                if (letter == conversionTable[i][j]) {
-                    if (conversionTable == letterValues) {
-                        score = (byte) (i + 1);
-                    } else {
-                        score = (byte) (2 * i + 8);
-                    }
-                }
+                applyCorrectConversion(letter, conversionTable, conversionTable[i][j], i);
             }
         }
-        return score;
     }
 
-    public byte applyWordBonus(byte score) {
-        if (this.doubleWord) {
-            score *= 2;
-        } else if (this.tripleWord) {
-            score *= 3;
+    public void applyCorrectConversion(char letter, char[][] conversionTable,
+                                       char value, byte index) {
+        if (letter == value && conversionTable == letterValues) {
+            this.tempLetterScore = (byte) (index + 1);
+        } else if (letter == value && conversionTable == letterValues2) {
+            this.tempLetterScore = (byte) (2 * index + 8);
         }
-        return score;
+    }
+
+    public boolean applyLetterBonus() {
+        return this.doubleLetters != null && this.tripleLetters != null;
+    }
+
+    public void computeLetterBonus(char letter) {
+        if (applyCorrectLetterBonus(letter, this.doubleLetters)) {
+            this.tempLetterScore *= 2;
+        } else if (applyCorrectLetterBonus(letter, this.tripleLetters)) {
+            this.tempLetterScore *= 3;
+        }
     }
 
     public boolean applyCorrectLetterBonus(char letter,
-                                       Character[] letterBonusArr) {
+                                           Character[] letterBonusArr) {
         for (byte i = 0; i < letterBonusArr.length; i++) {
             if (letter == letterBonusArr[i]) {
                 letterBonusArr[i] = '\u0000';
@@ -112,7 +119,11 @@ public class Scrabble {
         return false;
     }
 
-    public boolean applyLetterBonus(char letter) {
-        return this.doubleLetters != null && this.tripleLetters != null;
+    public void applyWordBonus() {
+        if (this.doubleWord) {
+            this.score *= 2;
+        } else if (this.tripleWord) {
+            this.score *= 3;
+        }
     }
 }
